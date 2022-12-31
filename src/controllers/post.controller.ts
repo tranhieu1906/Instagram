6059801ef92887c623f90019caa0a524f908dc3b
Tran Hieu <nguyentranhieugttn@gmail.com>
2022-12-31 16:51:21 +0700

    delete post

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,7 +1,6 @@
 import { Posts } from "./../model/Posts";
 import createError from "http-errors";
 import { AppDataSource } from "../config/data-source";
-import { createQueryBuilder } from "typeorm";
 const Post = AppDataSource.getRepository(Posts);
 
 class PostController {
@@ -16,6 +15,29 @@ class PostController {
       res.status(201).json({
         success: true,
         post,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async deletePost(req, res, next) {
+    try {
+      const post = await Post.findOne({
+        where: { id: req.params.id },
+        relations: {
+          user: true,
+        },
+      });
+      if (!post) {
+        return next(createError(401, "Post Not Found"));
+      }
+      if (post.user.id !== req.user.id) {
+        return next(createError(401, "User Not Authenticated"));
+      }
+      await Post.delete({ id: req.params.id });
+      res.status(200).json({
+        success: true,
+        message: "Post Deleted",
       });
     } catch (error) {
       next(error);
--- src/router/postRouter.ts
+++ src/router/postRouter.ts
@@ -5,3 +5,5 @@ import Posts from "../controllers/post.controller";
 export const Post = express.Router();
 
 Post.post("/post/new", uploadPost.single("post"), Posts.newPost);
+
+Post.delete("/post/:id", Posts.deletePost);
