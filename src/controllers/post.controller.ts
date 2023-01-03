6f5c3bcc4d8ba79295cb20e314a10fb0815664f2
Tran Hieu <nguyentranhieugttn@gmail.com>
2023-01-03 22:34:26 +0700

    comment

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,3 +1,4 @@
+import { Comment } from "./../model/Comment";
 import { Follow } from "../model/Follow";
 import { User } from "../model/User";
 import { Post } from "../model/Post";
@@ -8,6 +9,7 @@ const { deleteFile } = require("../utils/awsFunctions");
 const PostRepo = AppDataSource.getRepository(Post);
 const UserRepo = AppDataSource.getRepository(User);
 const LikeRepo = AppDataSource.getRepository(Like);
+const CommentRepo = AppDataSource.getRepository(Comment);
 
 class PostController {
   // add posts
@@ -99,7 +101,7 @@ class PostController {
         where: { post: { id: req.params.id }, user: { id: req.user.id } },
       });
       if (like) {
-        LikeRepo.delete({
+        await LikeRepo.delete({
           post: req.params.id,
           user: req.user.id,
         });
@@ -108,7 +110,7 @@ class PostController {
           message: "Delete Like",
         });
       } else {
-        LikeRepo.save({
+        await LikeRepo.save({
           post: { id: req.params.id },
           user: { id: req.user.id },
         });
@@ -117,6 +119,77 @@ class PostController {
           message: "Create Like ",
         });
       }
+    } catch (error) {
+      next(error);
+    }
+  }
+  // Add Comment
+  async newComment(req, res, next) {
+    try {
+      const post = await PostRepo.findOne({
+        where: { id: req.params.id },
+      });
+      if (!post) {
+        return next(createError(401, "Post Not Found"));
+      }
+      const comment = await CommentRepo.save({
+        user: req.user.id,
+        comment_text: req.body.comment,
+        post: req.params.id,
+      });
+      res.status(201).json({
+        success: true,
+        comment,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  //delete comment
+  async DeleteComment(req, res, next) {
+    try {
+      const comment = await CommentRepo.findOne({
+        where: { id: req.params.id },
+        relations: {
+          user: true,
+        },
+      });
+      if (!comment) {
+        return next(createError(401, "Post Not Found"));
+      }
+      if (comment.user.id !== req.user.id) {
+        return next(createError(401, "User Not Authenticated"));
+      }
+      await CommentRepo.delete({ id: req.params.id });
+      res.status(200).json({
+        success: true,
+        message: "Comment Delete Success",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  // update comment
+  async updateComment(req, res, next) {
+    try {
+      const comment = await CommentRepo.findOne({
+        where: { id: req.params.id },
+        relations: {
+          user: true,
+        },
+      });
+      if (!comment) {
+        return next(createError(401, "comment Not Found"));
+      }
+      if (comment.user.id !== req.user.id) {
+        return next(createError(401, "User Not Authenticated"));
+      }
+      comment.comment_text = req.body.comment;
+      await CommentRepo.save(comment);
+      res.status(200).json({
+        success: true,
+        message: "Comment Updated",
+      });
     } catch (error) {
       next(error);
     }
