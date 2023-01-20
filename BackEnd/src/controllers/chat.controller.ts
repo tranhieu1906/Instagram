6ce35a5d240619c7d8a7bdad13e6490cc58c3142
ng-dao-29 <112545575+ng-dao-29@users.noreply.github.com>
2023-01-20 07:40:06 +0700

    pull master

--- BackEnd/src/controllers/chat.controller.ts
+++ BackEnd/src/controllers/chat.controller.ts
@@ -1,9 +1,45 @@
 import { User } from "../model/User";
 import { Rooms} from "../model/Room";
+import { Messages } from "../model/Messages";
 import { AppDataSource } from "../config/data-source";
 const UserRepository = AppDataSource.getRepository(User);
 const RoomRepository = AppDataSource.getRepository(Rooms);
+const MessageRepository = AppDataSource.getRepository(Messages)
 class Chat {
+
+    async getDataChat(req, res) {
+        try {
+            console.log(req.params.id)
+            let user = req.user.data
+            let chatId = req.params.id;
+            let dataChat = await RoomRepository.find({
+                relations: {
+                    users: true,
+                },
+                where: {id : chatId}
+            });
+
+            let dataMessage = await MessageRepository.find({
+                relations: {
+                    author: true
+                },
+                where: {
+                    room: dataChat
+                }
+            })
+
+            res.status(200).json({
+                success: true,
+                dataChat: dataChat[0],
+                dataMessage: dataMessage,
+            })
+        }catch (err) {
+            res.status(500).json({
+                success: false,
+                message: err.message
+            })
+        }
+    }
 
     async findCreateChat(req, res) {
         try {
